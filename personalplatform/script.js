d60e57f7371169aed08c13608b6c495ffd77c96c
Ismet Hamzaj <81379437+IsmetHamzaj@users.noreply.github.com>
2022-11-12 23:36:08 +0100

    Add files via upload

---
+++ personalplatform/script.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".typing", {
+    strings:["","Web Developer","Front-End Developer", "Back-End Developer", "Full-Stack Developer", "Freelancer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop: true
+})
